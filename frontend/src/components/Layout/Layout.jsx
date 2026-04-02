d6158b7912771aaa3f9a486e2482ad4d0f2d6505
TemplatesHub <dionisio.s.p.junior@icloud.com>
2026-04-02 16:11:47 -0300

    try

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -4,8 +4,8 @@ export default function Layout({ children }) {
   return (
     <div className="flex min-h-screen bg-gray-50">
       <Sidebar />
-      <main className="lg:ml-64 flex-1 overflow-auto pt-16 lg:pt-0">
-        <div className="p-6 lg:p-8">
+      <main className="lg:ml-64 flex-1 overflow-x-auto pt-16 lg:pt-0">
+        <div className="p-6 lg:p-8 min-h-screen">
           {children}
         </div>
       </main>
